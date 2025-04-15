1b195249679ec6982e6a7e81cacd63746dab1e97
veronica <220711948@students.uajy.ac.id>
2025-04-15 21:34:13 +0700

    Update data_loader.py

--- singlelabel/utils/data_loader.py
+++ singlelabel/utils/data_loader.py
@@ -7,5 +7,5 @@ def load_data():
     """
     Load and cache the dataset
     """
-    df = pd.read_csv('data/train_preprocess.csv')
+    df = pd.read_csv('singlelabel/data/train_preprocess.csv')
     return df
